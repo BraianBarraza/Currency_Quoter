645766a53ed5b8de7f28aacb96144d4e715589fc
braianbarraza <braian_019@hotmail.com>
2025-03-27 22:10:38 +0100

    API data formatted

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,50 @@
 <script setup>
-import {ref, reactive, onMounted} from "vue";
+import { ref, reactive, onMounted } from "vue";
 import Alert from "./components/Alert.vue";
 
 const currencies = ref([
-  {code: "USD", text: "American Dollar"},
-  {code: "EUR", text: "Euro"},
-  {code: "GBP", text: "Pound"},
+  { code: "USD", text: "American Dollar" },
+  { code: "EUR", text: "Euro" },
+  { code: "GBP", text: "Pound" },
 ]);
-const error = ref('')
+const error = ref("");
 
 const cryptocurrencies = ref([]);
-const quotation = reactive({
-  currency: '',
-  cryptocurrency: ''
-})
+const quote = reactive({
+  currency: "",
+  cryptocurrency: "",
+});
 
+const quotation = ref({});
 
 onMounted(() => {
   const url =
-      "https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD";
+    "https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD";
   fetch(url)
-      .then((response) => response.json())
-      .then(({Data}) => {
-        //destructuring for access to the Data Object
-        cryptocurrencies.value = Data.LIST;
-      });
+    .then((response) => response.json())
+    .then(({ Data }) => {
+      //destructuring for access to the Data Object
+      cryptocurrencies.value = Data.LIST;
+    });
 });
 
 const quoteCrypto = () => {
-
-  if (Object.values(quotation).includes('')) {
-    error.value = 'All fields must be filled'
-    return
+  if (Object.values(quote).includes("")) {
+    error.value = "All fields must be filled";
+    return;
   }
-  error.value = ''
-  getCryptoQuotation()
-}
+  error.value = "";
+  getCryptoQuotation();
+};
 
 const getCryptoQuotation = async () => {
-  const {currency, cryptocurrency} = quotation
-  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptocurrency}&tsyms=${currency}&sign=true`
-  console.log(url)
-  const response = await fetch(url)
-  const data = await response.json()
+  const { currency, cryptocurrency } = quote;
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptocurrency}&tsyms=${currency}&sign=true`;
+  const response = await fetch(url);
+  const data = await response.json();
 
-  console.log(data.DISPLAY[cryptocurrency])
-}
+  quotation.value = data.DISPLAY[cryptocurrency][currency];
+};
 </script>
 
 <template>
@@ -53,17 +52,13 @@ const getCryptoQuotation = async () => {
     <h1 class="title"><span>Cryptocurrency</span> Quoter System</h1>
 
     <div class="content">
-      <Alert v-if="error"
-      >
+      <Alert v-if="error">
         {{ error }}
-
       </Alert>
-      <form class="form"
-            @submit.prevent="quoteCrypto">
+      <form class="form" @submit.prevent="quoteCrypto">
         <div class="field">
           <label for="currency">Currency:</label>
-          <select id="currency"
-                  v-model="quotation.currency">
+          <select id="currency" v-model="quote.currency">
             <option value="">-- Select --</option>
 
             <option v-for="currency in currencies" :value="currency.code">
@@ -74,18 +69,49 @@ const getCryptoQuotation = async () => {
 
         <div class="field">
           <label for="cryptocurrency">Crypto:</label>
-          <select id="cryptocurrency"
-                  v-model="quotation.cryptocurrency">
+          <select id="cryptocurrency" v-model="quote.cryptocurrency">
             <option value="">-- Select --</option>
 
-            <option v-for="cryptocurrency in cryptocurrencies" :value="cryptocurrency.SYMBOL">
+            <option
+              v-for="cryptocurrency in cryptocurrencies"
+              :value="cryptocurrency.SYMBOL"
+            >
               {{ cryptocurrency.NAME }}
             </option>
           </select>
         </div>
 
-        <input type="submit" value="Quote Crypto"/>
+        <input type="submit" value="Quote Crypto" />
       </form>
+
+      <div v-if="quotation.length > 0" class="result-container">
+        <h2>Quotation</h2>
+
+        <div class="result">
+          <img
+            :src="'https://www.cryptocompare.com/' + quotation.IMAGEURL"
+            :alt="'Image Crypto' + quotation.NAME"
+          />
+          <div>
+            <p>
+              Actual Price: <span>{{ quotation.PRICE }}</span>
+            </p>
+            <p>
+              Highest price of the day: <span>{{ quotation.HIGHDAY }}</span>
+            </p>
+            <p>
+              Lowest price of the day: <span>{{ quotation.LOWDAY }}</span>
+            </p>
+            <p>
+              Last 24 hours variation:
+              <span>{{ quotation.CHANGEPCTHOUR }} %</span>
+            </p>
+            <p>
+              Last actualization: <span>{{ quotation.LASTUPDATE }}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
